e60425013640518ea4ad70f37549bb4bd4058fe0
Doniyor <doniyormurodov246810@gmail.com>
2024-09-20 20:33:49 +0500

    component ochdim

---
+++ Components/Doniyor/Doniyor.jsx
@@ -0,0 +1,22 @@
+
+const Doniyor = () => {
+  return (
+    <>
+  <div className="navbar">
+    <div className="left">
+        <p>A creative agency based in Helsinki</p>
+        <p>hello@robi.com</p>
+    </div>
+    <div className="right">
+      <Link>Home</Link>
+      <Link>About</Link>
+      <Link>News</Link>
+      <Link>Contact</Link>
+      <Link>Cart</Link>
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default Doniyor
